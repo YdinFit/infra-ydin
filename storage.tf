1367aaf5fdf616762a68d65736d7acb718fb8fdf
Dusan Turajlic <dusan@turajlic.fi>
2026-04-06 22:17:23 +0300

    Fixes formatting form in teraform

--- storage.tf
+++ storage.tf
@@ -4,23 +4,26 @@ resource "cloudflare_r2_bucket" "data" {
   location   = "WEUR"
 }
 
-resource "cloudflare_r2_bucket_domain" "catalog" {
+resource "cloudflare_r2_custom_domain" "catalog" {
   account_id  = var.cloudflare_account_id
   bucket_name = cloudflare_r2_bucket.data.name
   zone_id     = var.cloudflare_zone_id
   domain      = "catalog.ydin.app"
+  enabled     = true
 }
 
 resource "cloudflare_ruleset" "r2_access_control" {
-  zone_id     = var.cloudflare_zone_id
-  name        = "R2 path access control"
-  kind        = "zone"
-  phase       = "http_request_firewall_custom"
+  zone_id = var.cloudflare_zone_id
+  name    = "R2 path access control"
+  kind    = "zone"
+  phase   = "http_request_firewall_custom"
 
-  rules {
-    action      = "block"
-    description = "Only allow /public/ folder on catalog.ydin.app"
-    expression  = "(http.host eq \"catalog.ydin.app\" and not starts_with(http.request.uri.path, \"/public/\"))"
-    enabled     = true
-  }
+  rules = [
+    {
+      action      = "block"
+      description = "Only allow /public/ folder on catalog.ydin.app"
+      expression  = "(http.host eq \"catalog.ydin.app\" and not starts_with(http.request.uri.path, \"/public/\"))"
+      enabled     = true
+    }
+  ]
 }
